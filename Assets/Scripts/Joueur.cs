91efb7b38994f20acca952dd9a90c64b8076c48c
SimRous <simonrouge1237@gmail.com>
2024-02-22 17:00:05 -0500

    cube
    jai ajouter un cube

--- Assets/Scripts/Joueur.cs
+++ Assets/Scripts/Joueur.cs
@@ -7,8 +7,7 @@ public class Joueur : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Message du futur
-        //my god
+        
     }
 
     // Update is called once per frame
